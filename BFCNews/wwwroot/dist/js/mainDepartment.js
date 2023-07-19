efecef39b0d74a556e69dd13252c6dab09609026
quockhanh172 <101686329+quockhanh172@users.noreply.github.com>
2023-07-19 23:17:15 +0700

    sửa lại cái active

--- BFCNews/wwwroot/dist/js/mainDepartment.js
+++ BFCNews/wwwroot/dist/js/mainDepartment.js
@@ -1,32 +1,15 @@
-const queryString = window.location.pathname;
-var sidebar = $("#sidebarAdmin li");
-function getPath() {
-    var n = "";
-    for (var i = queryString.length - 1; i >= 0; i--) {
-        if (queryString[i] === "/") {
-            var a = queryString.slice(i + 1, queryString.length);
-            n += a;
-            break;
-        }
-    }
-    return n;
-}
+$(document).ready(function () {
+    const activePage = window.location.pathname;
 
-window.onload = (event) => {
-    for (var i = 0; i < sidebar.length; i++) {
-        if ($(sidebar[i]).children().data("id")===(getPath())) {
-           $(sidebar[i]).children("a").addClass("active");
+    const navLinks = $(".nav-link");
+
+    navLinks.each((element,value) => {
+        var a = $(value);
+        if (a.attr('href').indexOf(activePage) !== -1) {
+            a.addClass('active');
         }
-    }
-    for (var i = queryString.length-1; i >=0 ; i--) {
-        if (queryString[i] === "/") {
-           var a= queryString.slice(i+1, queryString.length);
-            $("#destinationPage").html(a);
-            $("#dPage").html(a);
-            break;
-        }
-    }
-};
+    });
+});
 
     $(document).ready(function () {
         $("#addDepartment").click(() => {
